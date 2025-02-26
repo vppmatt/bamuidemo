8075f8e0f045b25da788dc361fddfe2f6e7beb5d
Matt <matt.thornfield@neueda.com>
2025-02-26 12:52:46 +0000

    environments

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../model/User';
 import { Observable, Subscription } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,9 @@ export class DataService {
 
   constructor(private http : HttpClient) { }
 
+  url = environment.serverUrl;
+
   getUsers() : Observable<Array<User>> {
-    return this.http.get<User[]>("http://localhost:8080/api/user");
+    return this.http.get<User[]>(`${this.url}/api/user`);
   }
 }
