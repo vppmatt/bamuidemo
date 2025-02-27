7005c49db086b2bf4b4c91f86a8cbbf47d3d0f96
Matt <matt.thornfield@neueda.com>
2025-02-27 11:39:48 +0000

    aggrid not working

--- src/app/access-records/access-records.component.ts
+++ src/app/access-records/access-records.component.ts
@@ -2,21 +2,44 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { AccessRecord } from '../../model/AccessRecord';
 import { NgFor } from '@angular/common';
+import  {AgGridAngular} from 'ag-grid-angular';
+import { ColDef } from 'ag-grid-community';
+
+//TO DO - REVIEW 
+
+import { ModuleRegistry } from 'ag-grid-community'; 
+import { ClientSideRowModelModule } from 'ag-grid-community'; 
+
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-quartz.css'
 
 @Component({
   selector: 'app-access-records',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor,AgGridAngular],
   templateUrl: './access-records.component.html',
   styleUrl: './access-records.component.css'
 })
 export class AccessRecordsComponent implements OnInit {
+
+  columnDefs : ColDef[] = [
+    {field : "id"},
+    {field : "user", valueGetter : params => {
+      return `${params.data.user.firstname} ${params.data.user.surname}`
+    } },
+    {field : "time"},
+    {field : "building"},
+    {field : "status", headerName: "direction",
+      valueGetter : params => params.data.status ? "in" : "out"
+    }
+  ];
 
   constructor(private dataService : DataService) {}
 
   accessRecords : AccessRecord[] = [];
 
   ngOnInit() {
+    ModuleRegistry.registerModules([ ClientSideRowModelModule ]); 
       this.dataService.getAccessRecords("20250225")
           .subscribe(data => this.accessRecords = data)
   }
